7b78902b5e063309c1d16726a5f8d69904df5bb2
DannyM15 <merinodanny15@gmail.com>
2024-07-24 20:00:38 -0700

    added icons and fixing navbar

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -2,11 +2,11 @@
 const HomePage = () => {
     return (
         <>
-            <h1 className=" ">MOONSHINE ANIMATIONS</h1>
+            <h1 className=" ">MOONSHINE</h1>
+            <h1 className=" ">ANIMATIONS</h1>
             <div className="d-flex ">
                 <div className="card p-2 flex-grow-1 ">
-                    <p>Youtube Video</p>
-                    <iframe  width="1000" height="800" src="https://www.youtube.com/embed/w5aH4g_U0PY?si=QTrAuHPtoeAhyhM3" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>                </div>
+                    <iframe  width="516" height="315" src="https://www.youtube.com/embed/w5aH4g_U0PY?si=QTrAuHPtoeAhyhM3" title="YouTube video player" frameborder="10" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>                </div>
 
                 <div className="card p-2 h-100">
                     <p>Vlog/Blog</p>
